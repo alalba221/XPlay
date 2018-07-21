c870b102577826cbb65ddae1d2f7540b69c484b8
zy <xinhua1413@gmail.com>
2018-07-21 00:33:30 -0500

    Concealed the Send() and Receive() into FFDecode

--- app/src/main/cpp/FFDecode.cpp
+++ app/src/main/cpp/FFDecode.cpp
@@ -22,6 +22,7 @@ bool FFDecode::Open(XParameter para){
     codec = avcodec_alloc_context3(cd);
     avcodec_parameters_to_context(codec,p);
 
+    codec->thread_count=8; //多线程解码
     //3. 打开解码器
     int re = avcodec_open2(codec,0,0);
     if(re != 0){
@@ -32,4 +33,39 @@ bool FFDecode::Open(XParameter para){
     }
     XLOGI("avcodec_open2 success");
     return true;
+}
+
+//future 模型， 发送数据到线程解码
+bool FFDecode::SendPacket(XData pkt){
+
+    if(pkt.size<=0 || !pkt.data) return false;
+    if(!codec){
+        return false;
+    }
+    int re = avcodec_send_packet(codec,(AVPacket*)pkt.data);
+    if(re !=0){
+        return false;
+    }
+
+    return true;
+}
+//从线程中获取解码结果
+XData FFDecode::RecvFrame(){
+
+    if(!codec){
+        return XData();
+    }
+    if(!frame){
+        frame = av_frame_alloc();
+    }
+    int re = avcodec_receive_frame(codec,frame);
+    if(re!=0){
+        return XData();
+    }
+    XData d;
+    d.data = (unsigned char*)frame;
+    if(codec->codec_type==AVMEDIA_TYPE_VIDEO)
+        d.size = (frame->linesize[0]+frame->linesize[1]+frame->linesize[2])*frame->height;
+    return d;
+    //return XData();
 }
--- app/src/main/cpp/IDecode.h
+++ app/src/main/cpp/IDecode.h
@@ -13,6 +13,11 @@ class IDecode: public IObserver {
 public:
     //打开解码器
     virtual bool Open(XParameter para)=0;
+
+    //future 模型， 发送一帧数据到线程解码
+    virtual bool SendPacket(XData pkt)=0;
+    //从线程中获取一帧解码结果
+    virtual XData RecvFrame()=0;
 };
 
 
