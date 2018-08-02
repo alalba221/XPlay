26624f41e7d4ee8792e0b657fcf8dd38115bf3d8
zy <xinhua1413@gmail.com>
2018-08-02 15:57:28 -0500

    Implement the thread safety for IDemux and FFdemux

--- app/src/main/cpp/FFDemux.cpp
+++ app/src/main/cpp/FFDemux.cpp
@@ -11,11 +11,22 @@ extern"C"{
 static double r2d(AVRational r){
     return r.num==0|| r.den==0? 0.:(double)r.num/(double)r.den;
 }
+
+void FFDemux::Close(){
+    mux.lock();
+    if(ic)
+        avformat_close_input(&ic);
+    mux.unlock();
+}
+
 //打开文件或者流媒体 rmtp,http,rtsp
 bool FFDemux::Open(const char* url){
     XLOGI("Open file %s begin",url);
+    Close();
+    mux.lock();
     int re = avformat_open_input(&ic,url,0,0);
     if(re!=0){
+        mux.unlock();
         char buf[1024] = {0};
         av_strerror(re, buf, sizeof(buf));
         XLOGE("FFDemux open %s failed!",url);
@@ -26,6 +37,7 @@ bool FFDemux::Open(const char* url){
     //读取文件信息
     re = avformat_find_stream_info(ic,0);
     if(re!=0){
+        mux.unlock();
         char buf[1024] = {0};
         av_strerror(re, buf, sizeof(buf));
         XLOGE("avformat_find_stream_info %s failed!",url);
@@ -34,6 +46,7 @@ bool FFDemux::Open(const char* url){
 
     this->totalMs = ic->duration/(AV_TIME_BASE/1000);
     XLOGI("total ms = %d",totalMs);
+    mux.unlock(); //GetVPara GetAPara里面可能还会有锁，锁两次会造成死锁
     // 就是为了获取音视频流index
     GetVPara();
     GetAPara();
@@ -43,12 +56,17 @@ bool FFDemux::Open(const char* url){
 
 //读取一帧数据，数据由调用者清理
 XData FFDemux::Read(){
-    if(!ic) return XData();
+    mux.lock();
+    if(!ic) {
+        mux.unlock();
+        return XData();
+    }
     XData d;
     AVPacket* pkt = av_packet_alloc();
     int re = av_read_frame(ic,pkt);
     if(re!=0){
         av_packet_free(&pkt);
+        mux.unlock();
         return XData();
     }
 //    XLOGI("pack size is %d, pts is %lld",pkt->size,pkt->pts);
@@ -61,6 +79,7 @@ XData FFDemux::Read(){
         d.isAudio = false;
     }else{
         av_packet_free(&pkt);
+        mux.unlock();
         return XData();
     }
 
@@ -69,35 +88,43 @@ XData FFDemux::Read(){
     pkt->dts = pkt->dts * (1000*r2d(ic->streams[pkt->stream_index]->time_base));
     d.pts = (int) pkt->pts;
     //XLOGE("demux pts is %d",d.pts);
+    mux.unlock();
     return d;
 }
 //获取视频参数
 XParameter FFDemux::GetVPara(){
+    mux.lock();
     if(!ic){
+        mux.unlock();
         XLOGE("GetVPara failed, ic is null");
         return XParameter();
     }
     //获取视频流索引
     int re = av_find_best_stream(ic,AVMEDIA_TYPE_VIDEO,-1,-1,0,0);
     if(re<0){
+        mux.unlock();
         XLOGE("av_find_best_stream failed");
         return XParameter();
     }
     videoStream = re;//通知解封装器 视频流的index
     XParameter para;
     para.para = ic->streams[re]->codecpar;
+    mux.unlock();
     return para;
 }
 
 //获取音频参数->音频解码器,重采样
 XParameter FFDemux::GetAPara(){
+    mux.lock();
     if(!ic){
+        mux.unlock();
         XLOGE("GetAPara failed, ic is null");
         return XParameter();
     }
     //获取音频流索引
     int re = av_find_best_stream(ic,AVMEDIA_TYPE_AUDIO,-1,-1,0,0);
     if(re<0){
+        mux.unlock();
         XLOGE("av_find_best_stream failed");
         return XParameter();
     }
@@ -106,6 +133,7 @@ XParameter FFDemux::GetAPara(){
     para.para = ic->streams[re]->codecpar;
     para.channels = ic->streams[re]->codecpar->channels;
     para.sample_rate = ic->streams[re]->codecpar->sample_rate;
+    mux.unlock();
     return para;
 }
 
--- app/src/main/cpp/FFDemux.h
+++ app/src/main/cpp/FFDemux.h
@@ -10,12 +10,13 @@
 //没有用引用的同文件，反而自己定义了这个结构体，设计模式参考了qt 因为是指针所以无所谓
 struct AVFormatContext;
 #include "IDemux.h"
-
+#include<mutex>
 class FFDemux: public IDemux {
 public:
 
     //打开文件或者流媒体 rmtp,http,rtsp
     virtual bool Open(const char* url);
+    virtual void Close();
     //读取一帧数据，数据由调用者清理
     virtual XData Read();
 
@@ -27,6 +28,7 @@ public:
 
 private:
     AVFormatContext* ic = 0;
+    std::mutex mux;//针对Open Read 对ic的访问
     //音视频流的标号
     int audioStream = 1;
     int videoStream = 0;
